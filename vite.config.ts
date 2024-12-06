5ac88aabf36e89f975e89b853bdc5d96ed725ade
Valerii Omelnyk <valeraaomelnik@gmail.com>
2024-12-06 04:21:01 +0200

    add base url

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,12 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  base: '/vue-todo-app/',git commit -am 'add base url'
+  git push origin main
+  plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
